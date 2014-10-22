19fffdf1d47f4026b532f63eae0726b6ad3ffffa
Nikolay Radkov <nikolay.kirilov.radkov@gmail.com>
2014-10-22 23:59:20 +0300

    Delete useless class

---
+++ NinjaLaptops/NinjaLaptops.Models/User.cs
@@ -0,0 +1,27 @@
+﻿namespace NinjaLaptops.Models
+{
+    using System.Threading.Tasks;
+    using System.Security.Claims;
+    using System.Collections.Generic;
+
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+
+    public class User : IdentityUser
+    {
+        public ClaimsIdentity GenerateUserIdentity(UserManager<User> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = manager.CreateIdentity(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+
+        public Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<User> manager)
+        {
+            return Task.FromResult(GenerateUserIdentity(manager));
+        }
+
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
