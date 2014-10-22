73e0fbf3e075333ad723799ca34f9542f113a574
Dobri <dobromir.v.zlatkov@gmail.com>
2014-10-22 23:34:06 +0300

    Brand Model.

--- NinjaLaptops/NinjaLaptops.Models/Brand.cs
+++ NinjaLaptops/NinjaLaptops.Models/Brand.cs
@@ -1,13 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace NinjaLaptops.Models
+﻿namespace NinjaLaptops.Models
 {
+    using System.Collections.Generic;
+
     public class Brand
     {
-        //TODO
+        public Brand()
+        {
+            this.Products = new HashSet<Product>();
+        }
+
+        public int BrandId { get; set; }
+
+        public string BrandName { get; set; }
+
+        public virtual ICollection<Product> Products { get; set; }
     }
 }
