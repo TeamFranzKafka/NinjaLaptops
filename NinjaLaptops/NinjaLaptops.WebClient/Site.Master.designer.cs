3f7c92bbec9d25552050dda27bbfd774bee8810a
Dobri <dobromir.v.zlatkov@gmail.com>
2014-10-23 11:26:32 +0300

    Error and success notifier

--- NinjaLaptops/NinjaLaptops.WebClient/Site.Master.designer.cs
+++ NinjaLaptops/NinjaLaptops.WebClient/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace NinjaLaptops.WebClient {
     public partial class SiteMaster {
         
         /// <summary>
+        /// ErrorSuccessNotifier control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Error_Handler_Control.ErrorSuccessNotifier ErrorSuccessNotifier;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
