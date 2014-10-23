e0c93391a3593bb9f1a2aa498f0e473ce0cd5891
Dobri <dobromir.v.zlatkov@gmail.com>
2014-10-23 21:35:53 +0300

    fixed EditProduct

---
+++ NinjaLaptops/NinjaLaptops.WebClient/AdminControllers/EditProduct.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="EditProduct.aspx.cs" Inherits="NinjaLaptops.WebClient.AdminControllers.EditProduct" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <h2>Edit Laptop</h2>
+
+    <div class="row">
+        <div class="col-md-8">
+            <section id="laptopForm">
+                <div class="form-horizontal">
+                    <asp:PlaceHolder runat="server" ID="ErrorMessage" Visible="false">
+                        <p class="text-danger">
+                            <asp:Literal runat="server" ID="FailureText" />
+                        </p>
+                    </asp:PlaceHolder>
+                    <div class="form-group">
+                        <asp:Label runat="server" AssociatedControlID="Model" CssClass="col-md-2 control-label">Model:</asp:Label>
+                        <div class="col-md-10">
+                            <asp:TextBox runat="server" ID="Model" CssClass="form-control" />  
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label runat="server" AssociatedControlID="Price" CssClass="col-md-2 control-label">Price:</asp:Label>
+                        <div class="col-md-10">
+                            <asp:TextBox runat="server" ID="Price" CssClass="form-control" />             
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label runat="server" AssociatedControlID="DropDownListBrands" CssClass="col-md-2 control-label">Brand:</asp:Label>
+                        <div class="col-md-10">
+                            <asp:DropDownList runat="server" ID="DropDownListBrands" DataTextField="BrandName" DataValueField="BrandId" CssClass="form-control" />          
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <asp:Label runat="server" AssociatedControlID="UploadImage" CssClass="col-md-2 control-label">Image:</asp:Label>
+                        <asp:FileUpload ID="UploadImage" runat="server" />   
+                    </div>
+                    <div class="form-group">
+                        <div class="col-md-offset-2 col-md-10">
+                            <asp:Button runat="server" OnClick="Updade_Click" Text="Update" CssClass="btn btn-default" />
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+</asp:Content>
---
+++ NinjaLaptops/NinjaLaptops.WebClient/AdminControllers/EditProduct.aspx.cs
@@ -0,0 +1,73 @@
+﻿using Error_Handler_Control;
+using NinjaLaptops.Data;
+using NinjaLaptops.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace NinjaLaptops.WebClient.AdminControllers
+{
+    public partial class EditProduct : System.Web.UI.Page
+    {
+        private NinjaLaptopsData data;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            this.data = new NinjaLaptopsData();
+
+            var brands = data.Brands.All().ToList();
+            this.DropDownListBrands.DataSource = brands;
+            this.DropDownListBrands.DataBind();
+        }
+
+        protected void Updade_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                int selectedLaptopId = int.Parse(Request.Params["productId"]);
+                var product = data.Products.GetById(selectedLaptopId);
+
+                if (this.UploadImage.HasFile)
+                {
+                    if (this.UploadImage.PostedFile.ContentType == "image/jpeg" ||
+                       this.UploadImage.PostedFile.ContentType == "image/gif" ||
+                       this.UploadImage.PostedFile.ContentType == "image/png")
+                    {
+                        
+                     
+
+                    
+                        var filename = product.Model + Path.GetExtension(this.UploadImage.FileName);
+                        this.UploadImage.SaveAs(this.Server.MapPath("~/Uploads/Images/") + filename);
+                        product.PictureLink = "Uploads/Images/" + filename;
+
+                    
+                    }
+                }
+                product.BrandId = int.Parse(this.DropDownListBrands.SelectedValue);
+                if (this.Model.Text != "")
+                {
+                    product.Model = this.Model.Text;
+                }
+               
+                if (this.Price.Text != "")
+                {
+                    var price = decimal.Parse(this.Price.Text);
+                    if (price < 0)
+                    {
+                        ErrorSuccessNotifier.AddErrorMessage("Product price cannot be negative!");
+                        return;
+                    }
+                    
+                    product.Price = decimal.Parse(this.Price.Text);
+                }
+                this.data.SaveChanges();
+                ErrorSuccessNotifier.AddSuccessMessage("Product updated successfully!");
+                Response.Redirect("/LaptopDetails?id=" + selectedLaptopId);
+            }
+        }
+    }
+}
